8877d4471504e0bfc3dd9b64acfe8003f7f38633
Tibor Nagy <xnagytibor@gmail.com>
2019-03-08 18:51:45 +0100

    Preparation for LZ4, add error handler, remove generated binaries

--- 120hz.asm
+++ 120hz.asm
@@ -11,6 +11,14 @@
 
         * = $1000 "Main"
 start:
+        lda #<lz4_song_data;  sta lz4_src
+        lda #>lz4_song_data;  sta lz4_src + 1
+
+        lda #<song_data;  sta lz4_dst
+        lda #>song_data;  sta lz4_dst + 1
+
+        jsr lz4_decompress
+
         jsr player_init
         jsr irq_init
         jmp *
@@ -55,10 +63,8 @@ irq_init:
         sta $01
 
         // Set interrupt address
-        lda #<irq_handler
-        sta $FFFE
-        lda #>irq_handler
-        sta $FFFF
+        lda #<irq_handler;  sta $FFFE
+        lda #>irq_handler;  sta $FFFF
 
         cli
         rts
@@ -205,9 +211,8 @@ player_update_set_loop:
 
 player_update_handle_exit:
         // TODO
-!:
-        inc $0401
-        jmp !-
+        lda #$00
+        jsr raise_error
         // ---
         rts
 
@@ -218,4 +223,102 @@ player_update_end:
 
         * = $1300 "Song data"
 song_data:
-        .import binary "res/songs-sid/txr.bin"
+        .import binary "res/songs-sid/sys.bin"
+
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        * = $6000 "LZ4 decompressor"
+lz4_src:
+        .word $0000
+lz4_dst:
+        .word $0000
+
+lz4_decompress:
+        //lda #$01
+        //jmp raise_error
+        rts
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        .label error_string_pointer = $20
+
+        // A = error code
+raise_error:
+        // Retrieve string pointer from error code
+        tax
+        lda error_strings_lo, x;  sta error_string_pointer
+        lda error_strings_hi, x;  sta error_string_pointer + 1
+
+        // Set red border
+        lda #$02
+        sta $D020
+
+        // Set black background
+        lda #$00
+        sta $D021
+
+        // Fill screen RAM with spaces
+        // Fill color RAM with red
+        ldx #$00
+!:
+        lda #$20
+        sta $0400, x
+        sta $0500, x
+        sta $0600, x
+        sta $0700, x
+        lda #$02
+        sta $D800, x
+        sta $D900, x
+        sta $DA00, x
+        sta $DB00, x
+        inx
+        bne !-
+
+        // Print error label
+        ldx #$00 // Screen index
+        ldy #$00 // String index
+!:
+        lda error_string_label, y
+        beq !+
+        sta $0400, x
+        inx
+        iny
+        jmp !-
+!:
+
+        // Print error message
+        ldy #$00
+!:
+        lda (error_string_pointer), y
+        beq !+
+        sta $0400, x
+        inx
+        iny
+        jmp !-
+!:
+
+        jmp *
+
+error_strings_lo:
+        .byte <error_string_00
+        .byte <error_string_01
+error_strings_hi:
+        .byte >error_string_00
+        .byte >error_string_01
+
+.encoding "screencode_upper"
+
+error_string_label:
+        .text "ERROR: "
+        .byte $00
+error_string_00:
+        .text "UNIMPLEMENTED FEATURE"
+        .byte $00
+error_string_01:
+        .text "BOJLER ELADO"
+        .byte $00
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        * = $8000 "Compressed data"
+lz4_song_data:
+        .import binary "res/songs-sid/sys.lz4"
--- Makefile
+++ Makefile
@@ -5,12 +5,17 @@ EMULATOR=~/Downloads/C64Debugger
 PIT_BINS := $(wildcard res/songs-pit/*.bin)
 SID_BINS := $(patsubst res/songs-pit/%.bin, res/songs-sid/%.bin, $(PIT_BINS))
 
+SID_BINS_LZ4 := $(patsubst res/songs-pit/%.bin, res/songs-sid/%.lz4, $(PIT_BINS))
+
 .PHONY: clean
 
 res/songs-sid/%.bin: res/songs-pit/%.bin
 	cargo run --manifest-path ./tools/Cargo.toml --bin pit2sid -- --input $< --output $@
 
-120hz.prg: 120hz.asm $(SID_BINS)
+res/songs-sid/%.lz4: res/songs-sid/%.bin
+	lz4 -9 -f $< $@
+
+120hz.prg: 120hz.asm $(SID_BINS) $(SID_BINS_LZ4)
 	java -jar ${KICKASS} 120hz.asm
 
 run: 120hz.prg
@@ -18,3 +23,5 @@ run: 120hz.prg
 
 clean:
 	rm 120hz.prg 120hz.sym
+	rm $(SID_BINS)
+	rm $(SID_BINS_LZ4)
