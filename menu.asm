2b0b7adbf0af195adbab58e7c034ad4e1b7c8caa
Tibor Nagy <xnagytibor@gmail.com>
2019-03-13 01:29:05 +0100

    Implement menu graphics functions

--- menu.asm
+++ menu.asm
@@ -9,7 +9,13 @@ font_lz77:
 text_lz77:
         .import binary "res/texts/menu.lz77"
 
+        .const color_selected = $01
+        .const color_normal = $0F
 
+item_selected:
+        .byte $00
+item_playing:
+        .byte $00
 
 init:
         lda #$00
@@ -17,8 +23,8 @@ init:
         sta $D021
 
         ldx #$00
+        lda #color_normal
 !:
-        lda #$0F
         sta mem.color_ram + $0000, x
         sta mem.color_ram + $0100, x
         sta mem.color_ram + $0200, x
@@ -46,7 +52,63 @@ init:
         lda #>mem.text_ram;  sta lz77.target + 1
         jsr lz77.decompress
 
-        lda #$0E // Note symbol
-        sta mem.text_ram + 40 * 2 + 4
+        lda #$00
+        sta item_selected
+        sta item_playing
+        jsr set_playing_marker
+        jsr set_selection
 
+        //jsr clear_playing_marker
+        //inc item_playing
+        //jsr set_playing_marker
+
+        //jsr clear_selection
+        //inc item_selected
+        //jsr set_selection
+
+        rts
+
+item_text_offset_lo:
+        .fill 23, <(mem.text_ram + 40 * (i + 2) + 4)
+item_text_offset_hi:
+        .fill 23, >(mem.text_ram + 40 * (i + 2) + 4)
+item_color_offset_lo:
+        .fill 23, <(mem.color_ram + 40 * (i + 2) + 4)
+item_color_offset_hi:
+        .fill 23, >(mem.color_ram + 40 * (i + 2) + 4)
+
+        // Clobbers A, X, Y
+clear_playing_marker:
+        ldy #$20  // Space
+        jmp !draw+
+set_playing_marker:
+        ldy #$0E  // Note character
+!draw:
+        ldx item_playing
+        lda item_text_offset_lo, x
+        sta !addr+ + 1
+        lda item_text_offset_hi, x
+        sta !addr+ + 2
+        tya
+!addr:  sta.abs $0000
+        rts
+
+        // Clobbers A, X, Y
+clear_selection:
+        ldy #color_normal
+        jmp !draw+
+set_selection:
+        ldy #color_selected
+!draw:
+        ldx item_selected
+        lda item_color_offset_lo, x
+        sta !addr+ + 1
+        lda item_color_offset_hi, x
+        sta !addr+ + 2
+        tya
+        ldx #35  // Item width
+!loop:
+!addr:  sta.abs $0000, x
+        dex
+        bpl !loop-
         rts
